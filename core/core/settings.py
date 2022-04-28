a8e5594f16026f35762c3361483d58808879c5aa
linuxoidder <97731833+linuxoidder@users.noreply.github.com>
2022-04-28 14:45:47 +0300

    [UPD]: settings

--- core/core/settings.py
+++ core/core/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = str(os.environ.get('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = str(os.environ.get('DEBUG'))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -42,6 +42,9 @@ INSTALLED_APPS = [
 
     # 2nd part apps
     'course',
+
+    # 3rd part apps
+    'rest_framework',
 
 ]
 
@@ -86,6 +89,7 @@ DATABASES = {
         'PASSWORD': 'root',
         'HOST': 'localhost',
         'PORT': 5433,
+        'ATOMIC_REQUESTS': True,
     }
 }
 
@@ -122,6 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
