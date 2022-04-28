7ad2a9f7f4b10a1a0baabd45b05b24658b3d9743
linuxoidder <97731833+linuxoidder@users.noreply.github.com>
2022-04-28 14:20:46 +0300

    [ADD]: models 'Course' & 'Category'

--- core/course/models.py
+++ core/course/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+TYPE_CHOICES = (
+    ('online', 'ONLINE'),
+    ('offline', 'OFFLINE')
+)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        db_table = 'categories'
+        ordering = ['id']
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField()
+    description = models.TextField()
+    type = models.CharField(choices=TYPE_CHOICES,
+                            default='online',
+                            null=False,
+                            max_length=255
+                            )
+    price = models.IntegerField()
+    files = models.FileField(upload_to=f'courses/{title.name}')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    students = models.ManyToManyField(User, null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'course'
+        ordering = ['date']
+        verbose_name = 'course'
+        verbose_name_plural = 'courses'
+
+    def __str__(self):
+        return self.title
